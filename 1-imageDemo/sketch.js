c6a84aaf2ffee1140b8a7e7a174390b187756424
Almonds714 <45469631+Almonds714@users.noreply.github.com>
2020-02-12 15:19:16 -0600

    demo updates and interactive scene created

--- 1-imageDemo/sketch.js
+++ 1-imageDemo/sketch.js
@@ -6,6 +6,7 @@
 // - describe what you did to take this project "above and beyond"
 
 let airpods;
+let scalar = 0.1;
 
 function preload(){
   airpods = loadImage("assets/airpods.jpg");
@@ -19,5 +20,15 @@ function draw() {
   background(220);
   fill("lightblue");
   // rect(mouseX-50, mouseY-50, 100, 100);
-  image(airpods, mouseX, mouseY);
+  image(airpods, mouseX-scalar*airpods.width/2, mouseY-scalar*airpods.height/2, scalar*airpods.width, scalar*airpods.height);
+}
+
+function mouseWheel(event) {
+  if (event.delta < 0){
+    scalar *= 1.1;
+  }
+  else{
+    scalar *= 0.9;
+  }
+  print(event.delta);
 }
