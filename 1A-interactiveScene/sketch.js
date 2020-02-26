2e32be764abd9e7dd419e4536c2bafad0370ad09
Almonds714 <45469631+Almonds714@users.noreply.github.com>
2020-02-26 14:25:38 -0600

    Update sketch.js

--- 1A-interactiveScene/sketch.js
+++ 1A-interactiveScene/sketch.js
@@ -1,15 +1,17 @@
-// Project Title
-// Your Name
-// Date
+// Stacker: Mad Stax
+// Brody Crawford
+// February 25, 2020
 //
-// Extra for Experts:
+// Extra for Experts: Adding background music, change with mouse wheel and SFX on cube placements
 // - describe what you did to take this project "above and beyond"
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  console.log(windowWidth);
+  console.log("lightblue");
 }
 
 function draw() {
-  background(220);
+  background(0, 0, 100);
 }
