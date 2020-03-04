edf97c14ac50f4c6be03cfc7c57fe01b31792668
Almonds714 <45469631+Almonds714@users.noreply.github.com>
2020-03-04 15:20:29 -0600

    Update sketch.js

--- 1A-interactiveScene/sketch.js
+++ 1A-interactiveScene/sketch.js
@@ -13,13 +13,14 @@
 let cubeSize;
 let locationX;
 
-//Play Functions
+// Play Functions
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   consoleInfo(); //Gives information into console from page, temporary...?
   rectMode(CENTER);
   cubeSize = height/15;
+  noCursor();
 }
 
 function draw() {
@@ -32,25 +33,33 @@ function draw() {
 function drawCubeBackground(){
   noStroke();
   fill(51, 246, 255);
+
+  // Creates 3 squares at the bottom of the screen successfully but manually
   // rect(width/2, height-cubeSize/2, cubeSize, cubeSize);
   // rect(width/2-cubeSize, height-cubeSize/2, cubeSize, cubeSize);
   // rect(width/2+cubeSize, height-cubeSize/2, cubeSize, cubeSize);
 
-  // for (let i = )
+  // for (let i=15; i>0; i--){
+  //   rect(width/2, height-cubeSize/2, cubeSize, cubeSize);
+  //   rect(width/2-cubeSize, height-cubeSize/2, cubeSize, cubeSize);
+  //   rect(width/2+cubeSize, height-cubeSize/2, cubeSize, cubeSize);
+  // }
 
-
+  // Creates 3x15 squares through the middle of the screen unsuccessfully with 2 for loops
   // for (let locationY = cubeSize*15; locationY < 0; locationY -= cubeSize){
   //   for (let locationX = cubeSize*-1; locationX < cubeSize*2; locationX += cubeSize){
   //     rect(width/2+locationX, height-cubeSize/2+locationY, cubeSize, cubeSize);
   //   }
   //   locationX = cubeSize*-1;
-  // }
+  // } 
 
+  // Creates 3 squares at the bottom of the screen successfully with a for loop
   // for (let locationX = cubeSize*-1; locationX < cubeSize*2; locationX += cubeSize){
   //   rect(width/2+locationX, height-cubeSize/2, cubeSize, cubeSize);
   // }
 
-  stroke(0); //line in the middle for reference
+  //line in the middle for reference
+  stroke(0);
   line(width/2, 0, width/2, height);
 }
 
