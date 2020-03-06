fc8c600f91a72edb70792aac7bbc7fed074e0df0
Almonds714 <45469631+Almonds714@users.noreply.github.com>
2020-03-06 15:11:35 -0600

    getting going

--- 1A-interactiveScene/sketch.js
+++ 1A-interactiveScene/sketch.js
@@ -12,55 +12,49 @@
 
 let cubeSize;
 let locationX;
+let placement;
 
 // Play Functions
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   consoleInfo(); //Gives information into console from page, temporary...?
+
   rectMode(CENTER);
-  cubeSize = height/15;
-  noCursor();
+  cubeSize = height/15; //Do not change later on
 }
 
 function draw() {
-  background(66, 75, 245); //nice blue color - 66, 75, 245.  Lighter blue color - 51, 246, 255.  Almost white, blue color - 199, 251, 255.
+  background(199, 251, 255); //nice blue color - 66, 75, 245.  Lighter blue color - 51, 246, 255.  Almost white, blue color - 199, 251, 255.
   drawCubeBackground(); 
+  stack();
 }
 
 // Functions 
 
+function stack(){
+  // Draws first layer
+  fill(66, 75, 245);
+  for (let i=-1; i<2; i++){
+    rect(width/2+cubeSize*i, height-cubeSize/2, cubeSize, cubeSize);
+  }
+
+  
+}
+
 function drawCubeBackground(){
-  noStroke();
-  fill(51, 246, 255);
+  stroke(255);
+  fill(0);
 
-  // Creates 3 squares at the bottom of the screen successfully but manually
-  // rect(width/2, height-cubeSize/2, cubeSize, cubeSize);
-  // rect(width/2-cubeSize, height-cubeSize/2, cubeSize, cubeSize);
-  // rect(width/2+cubeSize, height-cubeSize/2, cubeSize, cubeSize);
-
-  // for (let i=15; i>0; i--){
-  //   rect(width/2, height-cubeSize/2, cubeSize, cubeSize);
-  //   rect(width/2-cubeSize, height-cubeSize/2, cubeSize, cubeSize);
-  //   rect(width/2+cubeSize, height-cubeSize/2, cubeSize, cubeSize);
-  // }
-
-  // Creates 3x15 squares through the middle of the screen unsuccessfully with 2 for loops
-  // for (let locationY = cubeSize*15; locationY < 0; locationY -= cubeSize){
-  //   for (let locationX = cubeSize*-1; locationX < cubeSize*2; locationX += cubeSize){
-  //     rect(width/2+locationX, height-cubeSize/2+locationY, cubeSize, cubeSize);
-  //   }
-  //   locationX = cubeSize*-1;
-  // } 
-
-  // Creates 3 squares at the bottom of the screen successfully with a for loop
-  // for (let locationX = cubeSize*-1; locationX < cubeSize*2; locationX += cubeSize){
-  //   rect(width/2+locationX, height-cubeSize/2, cubeSize, cubeSize);
-  // }
+  for (let i=0; i<16; i++){
+    for (let x=-4; x<5; x++){
+      rect(width/2+cubeSize*x, height-cubeSize/2-cubeSize*i, cubeSize, cubeSize);
+    }
+  }
 
   //line in the middle for reference
-  stroke(0);
-  line(width/2, 0, width/2, height);
+  // stroke(0);
+  // line(width/2, 0, width/2, height);
 }
 
 function consoleInfo(){
