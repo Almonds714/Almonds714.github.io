86c25015c501bd50fe597a19d193ec96a4a7a544
Almonds714 <45469631+Almonds714@users.noreply.github.com>
2020-03-11 15:13:04 -0600

    barely g for submission
    couldnt finish in time

--- 1A-interactiveScene/sketch.js
+++ 1A-interactiveScene/sketch.js
@@ -2,27 +2,24 @@
 // Brody Crawford
 // February 25, 2020
 //
-// Extra for Experts: Adding background music, change with mouse wheel and SFX on cube placements
+// Extra for Experts: 
 // - describe what you did to take this project "above and beyond"
-
-// Known Bugs: None
-// To Do; Create block placements using objects
 
 // Global Variables
 
 let cubeSize;
-let cubeState;
+// let cubeState;
 let stackerY;
 let stackerX;
 let stackerDX;
-let stackerSize;
+// let stackerSize;
 
 // Play Functions. Setup and Draw loop.
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   consoleInfo(); //Gives information into console from page, temporary...?
-  stackerTicSpeed=millis();
+  // stackerTicSpeed=millis();
 
   rectMode(CENTER);
   cubeSize=height/15; //Do not change later on
@@ -31,28 +28,17 @@ function setup() {
 
   stackerDX=5;
 
-  cubeState="3Cubes";
-  stackerSize=1;
+  // cubeState="3Cubes";
+  // stackerSize=1;
 }
 
 function draw() {
   background(199, 251, 255); //nice blue color - 66, 75, 245.  Lighter blue color - 51, 246, 255.  Almost white, blue color - 199, 251, 255.
   drawCubeBackground(); 
-  // initialStack();
   stack();
 }
 
-
 // Functions 
-
-// function initialStack(){
-//   // Draws first layer
-//   fill(66, 75, 245);
-//   for (let i=-1; i<2; i++){
-//     rect(width/2+cubeSize*i, height-cubeSize/2, cubeSize, cubeSize);
-//   }
-//   stackerX=width/2-cubeSize*4;
-// }
 
 function stack(){
   fill(66, 75, 245);
@@ -68,14 +54,15 @@ function stack(){
     stackerDX*=-1;
   }
 
-  if (mouseIsClicked){
-    stackerY+=cubeSize;
-    mouseIsClicked=false;
-  }
+  // if (mouseIsClicked){
+  //   stackerY+=cubeSize;
+  //   mouseIsClicked=false;
+  // }
 }
 
 function mouseClicked(){
-  mouseIsClicked=true;
+  // mouseIsClicked=true;
+  stackerY-=cubeSize;
 }
 
 function drawCubeBackground(){
