349e40f0df2cc05deda0c837c8540c85b3352705
Brody <45469631+Almonds714@users.noreply.github.com>
2020-03-11 04:57:34 -0600

    reee

--- 1A-interactiveScene/sketch.js
+++ 1A-interactiveScene/sketch.js
@@ -11,36 +11,71 @@
 // Global Variables
 
 let cubeSize;
-let stacker = [];
-
+let cubeState;
+let stackerY;
+let stackerX;
+let stackerDX;
+let stackerSize;
 
 // Play Functions. Setup and Draw loop.
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   consoleInfo(); //Gives information into console from page, temporary...?
+  stackerTicSpeed=millis();
 
   rectMode(CENTER);
-  cubeSize = height/15; //Do not change later on
+  cubeSize=height/15; //Do not change later on
+  stackerY=height;
+  stackerX=width/2;
+
+  stackerDX=5;
+
+  cubeState="3Cubes";
+  stackerSize=1;
 }
 
 function draw() {
   background(199, 251, 255); //nice blue color - 66, 75, 245.  Lighter blue color - 51, 246, 255.  Almost white, blue color - 199, 251, 255.
   drawCubeBackground(); 
+  // initialStack();
   stack();
 }
 
 
 // Functions 
 
+// function initialStack(){
+//   // Draws first layer
+//   fill(66, 75, 245);
+//   for (let i=-1; i<2; i++){
+//     rect(width/2+cubeSize*i, height-cubeSize/2, cubeSize, cubeSize);
+//   }
+//   stackerX=width/2-cubeSize*4;
+// }
+
 function stack(){
-  // Draws first layer
   fill(66, 75, 245);
-  for (let i=-1; i<2; i++){
-    rect(width/2+cubeSize*i, height-cubeSize/2, cubeSize, cubeSize);
+  rect(stackerX, stackerY-cubeSize/2, cubeSize*3, cubeSize);
+
+  // Move
+  stackerX+=stackerDX;
+  
+  if (stackerX>width/2+cubeSize*3){
+    stackerDX*=-1;
+  }
+  if (stackerX<width/2-cubeSize*3){
+    stackerDX*=-1;
   }
 
+  if (mouseIsClicked){
+    stackerY+=cubeSize;
+    mouseIsClicked=false;
+  }
+}
 
+function mouseClicked(){
+  mouseIsClicked=true;
 }
 
 function drawCubeBackground(){
@@ -58,4 +93,5 @@ function consoleInfo(){
   console.log(windowWidth);
   console.log(windowHeight);
   console.log(windowHeight/15);
+  console.log(stackerX);
 }
